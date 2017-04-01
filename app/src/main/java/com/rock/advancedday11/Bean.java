dc7c2850db7faf99552af644a01af9c90aacc7d9
Rock <rockrong1204@gmail.com>
2017-04-01 11:15:55 +0800

    使用GsonFormat生成JavaBean

---
+++ app/src/main/java/com/rock/advancedday11/Bean.java
@@ -0,0 +1,297 @@
+package com.rock.advancedday11;
+
+import java.util.List;
+
+/**
+ * Created by Rock on 2017/4/1.
+ */
+
+public class Bean {
+
+
+    /**
+     * status : 0
+     * msg : OK
+     * data : [{"postid":"51488","title":"惊呆！一场随性随缘的打劫","wx_small_app_title":"惊呆！一场随性随缘的打劫","pid":"1","app_fu_title":"","is_xpc":"0","is_promote":"0","is_xpc_zp":"0","is_xpc_cp":"0","is_xpc_fx":"0","is_album":"0","tags":"","recent_hot":"0","discussion":"0","image":"http://cs.vmoiver.com/Uploads/cover/2017-03-31/58de22c8129e8_cut.jpeg","rating":"8.0","duration":"592","publish_time":"1490976300","like_num":"53","share_num":"92","cates":[{"cateid":"17","catename":"剧情"}],"request_url":"http://app.vmoiver.com/51488?qingapp=app_new"},{"postid":"51487","title":"只怪人生太匆匆 爱成往事梦成空","wx_small_app_title":"只怪人生太匆匆 爱成往事梦成空","pid":"1","app_fu_title":"","is_xpc":"0","is_promote":"0","is_xpc_zp":"0","is_xpc_cp":"0","is_xpc_fx":"0","is_album":"0","tags":"","recent_hot":"0","discussion":"0","image":"http://cs.vmoiver.com/Uploads/cover/2017-03-31/58de741ac1855_cut.jpeg","rating":"7.5","duration":"333","publish_time":"1490976240","like_num":"122","share_num":"325","cates":[{"cateid":"44","catename":"混剪"}],"request_url":"http://app.vmoiver.com/51487?qingapp=app_new"},{"postid":"51458","title":"人生短暂 是时候出去浪了！","wx_small_app_title":"人生短暂 是时候出去浪了！","pid":"1","app_fu_title":"","is_xpc":"0","is_promote":"0","is_xpc_zp":"0","is_xpc_cp":"0","is_xpc_fx":"0","is_album":"0","tags":"","recent_hot":"0","discussion":"0","image":"http://cs.vmoiver.com/Uploads/cover/2017-03-29/58db6fd8ab47e_cut.jpeg","rating":"7.2","duration":"180","publish_time":"1490976180","like_num":"124","share_num":"165","cates":[{"cateid":"11","catename":"旅行"}],"request_url":"http://app.vmoiver.com/51458?qingapp=app_new"},{"postid":"51463","title":"西班牙动人迟暮爱情短片","wx_small_app_title":"西班牙动人迟暮爱情短片","pid":"1","app_fu_title":"","is_xpc":"0","is_promote":"0","is_xpc_zp":"0","is_xpc_cp":"0","is_xpc_fx":"0","is_album":"0","tags":"","recent_hot":"0","discussion":"0","image":"http://cs.vmoiver.com/Uploads/cover/2017-03-29/58db5ebaadebb_cut.jpeg","rating":"7.3","duration":"144","publish_time":"1490976120","like_num":"88","share_num":"93","cates":[{"cateid":"17","catename":"剧情"}],"request_url":"http://app.vmoiver.com/51463?qingapp=app_new"},{"postid":"51389","title":"吃货的人生无可阻挡","wx_small_app_title":"吃货的人生无可阻挡","pid":"1","app_fu_title":"","is_xpc":"0","is_promote":"0","is_xpc_zp":"0","is_xpc_cp":"0","is_xpc_fx":"0","is_album":"0","tags":"","recent_hot":"0","discussion":"0","image":"http://cs.vmoiver.com/Uploads/cover/2017-03-31/58de74b09e211_cut.jpeg","rating":"6.9","duration":"102","publish_time":"1490976060","like_num":"28","share_num":"28","cates":[{"cateid":"13","catename":"广告"}],"request_url":"http://app.vmoiver.com/51389?qingapp=app_new"},{"postid":"51346","title":"套路是我学的，但整你是真心的","wx_small_app_title":"套路是我学的，但整你是真心的","pid":"1","app_fu_title":"愚人节整蛊指南","is_xpc":"0","is_promote":"0","is_xpc_zp":"0","is_xpc_cp":"0","is_xpc_fx":"0","is_album":"1","tags":"","recent_hot":"0","discussion":"0","image":"http://cs.vmoiver.com/Uploads/cover/2017-03-31/58ddfe2254028_cut.jpeg","rating":"7.5","duration":"140","publish_time":"1490962792","like_num":"143","share_num":"449","cates":[{"cateid":"6","catename":"创意"}],"request_url":"http://app.vmoiver.com/51346?qingapp=app_new"},{"postid":"51486","title":"「猩球崛起3：终极之战」预告 道破人猿大战起因","wx_small_app_title":"「猩球崛起3：终极之战」预告 道破人猿大战起因","pid":"1","app_fu_title":"","is_xpc":"0","is_promote":"0","is_xpc_zp":"0","is_xpc_cp":"0","is_xpc_fx":"0","is_album":"0","tags":"","recent_hot":"0","discussion":"0","image":"http://cs.vmoiver.com/Uploads/cover/2017-03-31/58ddba7a1e612_cut.jpeg","rating":"7.8","duration":"146","publish_time":"1490926620","like_num":"176","share_num":"415","cates":[{"cateid":"43","catename":"预告"}],"request_url":"http://app.vmoiver.com/51486?qingapp=app_new"},{"postid":"51485","title":"「权力的游戏」第七季先导预告来袭！","wx_small_app_title":"「权力的游戏」第七季先导预告来袭！","pid":"1","app_fu_title":"","is_xpc":"0","is_promote":"0","is_xpc_zp":"0","is_xpc_cp":"0","is_xpc_fx":"0","is_album":"0","tags":"","recent_hot":"0","discussion":"0","image":"http://cs.vmoiver.com/Uploads/cover/2017-03-31/58ddb376311e0_cut.jpeg","rating":"7.0","duration":"92","publish_time":"1490925300","like_num":"103","share_num":"473","cates":[{"cateid":"43","catename":"预告"}],"request_url":"http://app.vmoiver.com/51485?qingapp=app_new"},{"postid":"51468","title":"GoPro震撼纪录短片「鹰猎人」","wx_small_app_title":"GoPro震撼纪录短片「鹰猎人」","pid":"1","app_fu_title":"","is_xpc":"0","is_promote":"0","is_xpc_zp":"0","is_xpc_cp":"0","is_xpc_fx":"0","is_album":"0","tags":"","recent_hot":"0","discussion":"0","image":"http://cs.vmoiver.com/Uploads/cover/2017-03-29/58db98a1b5086_cut.jpeg","rating":"8.0","duration":"1115","publish_time":"1490889900","like_num":"270","share_num":"356","cates":[{"cateid":"24","catename":"纪录"}],"request_url":"http://app.vmoiver.com/51468?qingapp=app_new"},{"postid":"51479","title":"爱情岔路口 你要如何选择","wx_small_app_title":"爱情岔路口 你要如何选择","pid":"1","app_fu_title":"","is_xpc":"0","is_promote":"0","is_xpc_zp":"0","is_xpc_cp":"0","is_xpc_fx":"0","is_album":"0","tags":"","recent_hot":"0","discussion":"0","image":"http://cs.vmoiver.com/Uploads/cover/2017-03-30/58dcf282112d3_cut.jpeg","rating":"7.9","duration":"596","publish_time":"1490889840","like_num":"604","share_num":"704","cates":[{"cateid":"12","catename":"爱情"}],"request_url":"http://app.vmoiver.com/51479?qingapp=app_new"}]
+     */
+
+    private String status;
+    private String msg;
+    private List<MovieBean> data;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public List<MovieBean> getData() {
+        return data;
+    }
+
+    public void setData(List<MovieBean> data) {
+        this.data = data;
+    }
+
+    public static class MovieBean {
+        /**
+         * postid : 51488
+         * title : 惊呆！一场随性随缘的打劫
+         * wx_small_app_title : 惊呆！一场随性随缘的打劫
+         * pid : 1
+         * app_fu_title :
+         * is_xpc : 0
+         * is_promote : 0
+         * is_xpc_zp : 0
+         * is_xpc_cp : 0
+         * is_xpc_fx : 0
+         * is_album : 0
+         * tags :
+         * recent_hot : 0
+         * discussion : 0
+         * image : http://cs.vmoiver.com/Uploads/cover/2017-03-31/58de22c8129e8_cut.jpeg
+         * rating : 8.0
+         * duration : 592
+         * publish_time : 1490976300
+         * like_num : 53
+         * share_num : 92
+         * cates : [{"cateid":"17","catename":"剧情"}]
+         * request_url : http://app.vmoiver.com/51488?qingapp=app_new
+         */
+
+        private String postid;
+        private String title;
+        private String wx_small_app_title;
+        private String pid;
+        private String app_fu_title;
+        private String is_xpc;
+        private String is_promote;
+        private String is_xpc_zp;
+        private String is_xpc_cp;
+        private String is_xpc_fx;
+        private String is_album;
+        private String tags;
+        private String recent_hot;
+        private String discussion;
+        private String image;
+        private String rating;
+        private String duration;
+        private String publish_time;
+        private String like_num;
+        private String share_num;
+        private String request_url;
+        private List<MovieCatesBean> cates;
+
+        public String getPostid() {
+            return postid;
+        }
+
+        public void setPostid(String postid) {
+            this.postid = postid;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getWx_small_app_title() {
+            return wx_small_app_title;
+        }
+
+        public void setWx_small_app_title(String wx_small_app_title) {
+            this.wx_small_app_title = wx_small_app_title;
+        }
+
+        public String getPid() {
+            return pid;
+        }
+
+        public void setPid(String pid) {
+            this.pid = pid;
+        }
+
+        public String getApp_fu_title() {
+            return app_fu_title;
+        }
+
+        public void setApp_fu_title(String app_fu_title) {
+            this.app_fu_title = app_fu_title;
+        }
+
+        public String getIs_xpc() {
+            return is_xpc;
+        }
+
+        public void setIs_xpc(String is_xpc) {
+            this.is_xpc = is_xpc;
+        }
+
+        public String getIs_promote() {
+            return is_promote;
+        }
+
+        public void setIs_promote(String is_promote) {
+            this.is_promote = is_promote;
+        }
+
+        public String getIs_xpc_zp() {
+            return is_xpc_zp;
+        }
+
+        public void setIs_xpc_zp(String is_xpc_zp) {
+            this.is_xpc_zp = is_xpc_zp;
+        }
+
+        public String getIs_xpc_cp() {
+            return is_xpc_cp;
+        }
+
+        public void setIs_xpc_cp(String is_xpc_cp) {
+            this.is_xpc_cp = is_xpc_cp;
+        }
+
+        public String getIs_xpc_fx() {
+            return is_xpc_fx;
+        }
+
+        public void setIs_xpc_fx(String is_xpc_fx) {
+            this.is_xpc_fx = is_xpc_fx;
+        }
+
+        public String getIs_album() {
+            return is_album;
+        }
+
+        public void setIs_album(String is_album) {
+            this.is_album = is_album;
+        }
+
+        public String getTags() {
+            return tags;
+        }
+
+        public void setTags(String tags) {
+            this.tags = tags;
+        }
+
+        public String getRecent_hot() {
+            return recent_hot;
+        }
+
+        public void setRecent_hot(String recent_hot) {
+            this.recent_hot = recent_hot;
+        }
+
+        public String getDiscussion() {
+            return discussion;
+        }
+
+        public void setDiscussion(String discussion) {
+            this.discussion = discussion;
+        }
+
+        public String getImage() {
+            return image;
+        }
+
+        public void setImage(String image) {
+            this.image = image;
+        }
+
+        public String getRating() {
+            return rating;
+        }
+
+        public void setRating(String rating) {
+            this.rating = rating;
+        }
+
+        public String getDuration() {
+            return duration;
+        }
+
+        public void setDuration(String duration) {
+            this.duration = duration;
+        }
+
+        public String getPublish_time() {
+            return publish_time;
+        }
+
+        public void setPublish_time(String publish_time) {
+            this.publish_time = publish_time;
+        }
+
+        public String getLike_num() {
+            return like_num;
+        }
+
+        public void setLike_num(String like_num) {
+            this.like_num = like_num;
+        }
+
+        public String getShare_num() {
+            return share_num;
+        }
+
+        public void setShare_num(String share_num) {
+            this.share_num = share_num;
+        }
+
+        public String getRequest_url() {
+            return request_url;
+        }
+
+        public void setRequest_url(String request_url) {
+            this.request_url = request_url;
+        }
+
+        public List<MovieCatesBean> getCates() {
+            return cates;
+        }
+
+        public void setCates(List<MovieCatesBean> cates) {
+            this.cates = cates;
+        }
+
+        public static class MovieCatesBean {
+            /**
+             * cateid : 17
+             * catename : 剧情
+             */
+
+            private String cateid;
+            private String catename;
+
+            public String getCateid() {
+                return cateid;
+            }
+
+            public void setCateid(String cateid) {
+                this.cateid = cateid;
+            }
+
+            public String getCatename() {
+                return catename;
+            }
+
+            public void setCatename(String catename) {
+                this.catename = catename;
+            }
+        }
+    }
+}
--- app/src/main/java/com/rock/advancedday11/MainActivity.java
+++ app/src/main/java/com/rock/advancedday11/MainActivity.java
@@ -1,7 +1,7 @@
 package com.rock.advancedday11;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.orhanobut.logger.Logger;
